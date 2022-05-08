178b2023bf271470109e56a053855dea5189ff58
Sameer Gupta <sameergupta4873@gmail.com>
2022-05-09 01:15:05 +0530

    user-authenication-done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/UsersModel');
+const generateToken = require('../utils/generateToken')
 const asyncHandler = require('express-async-handler');
 
 const authUser = asyncHandler(async(req,res)=>{
@@ -11,7 +12,7 @@ const authUser = asyncHandler(async(req,res)=>{
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }else{
         res.status(401)
